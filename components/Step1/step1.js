45c14021d45cccf12a87099a4ca09bbfbd262444
twxia <passion804222@gmail.com>
2017-03-11 18:58:37 +0800

    poll step1 & step runner done

---
+++ components/Step1/step1.js
@@ -0,0 +1,75 @@
+import React, { Component, PropTypes } from 'react';
+import { Grid, Cell, Textfield, Button } from 'react-mdl';
+
+class Step1 extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid>
+          <Cell col={12}>
+            <Textfield
+              onChange={this.handleChange}
+              label="Title"
+              name="title"
+              ref="title"
+              floatingLabel
+            />
+          </Cell>
+          <Cell col={12}>
+            <Textfield
+              onChange={this.handleChange}
+              label="Your Name"
+              name="name"
+              ref="name"
+              floatingLabel
+            />
+          </Cell>
+          <Cell col={12}>
+            <Textfield
+              onChange={this.handleChange}
+              label="E-mail"
+              name="email"
+              ref="email"
+              floatingLabel
+            />
+          </Cell>
+          <Cell col={6}>
+            <Textfield
+              onChange={this.handleChange}
+              label="Start Date (2055-12-1)"
+              name="start_date"
+              ref="startDate"
+              floatingLabel
+            />
+          </Cell>
+          <Cell col={6}>
+            <Textfield
+              onChange={this.handleChange}
+              label="End Date (2055-12-10)"
+              name="end_date"
+              ref="endDate"
+              floatingLabel
+            />
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+Step1.propTypes = {
+  startDate: React.PropTypes.string,
+  endDate: React.PropTypes.string,
+};
+
+export default Step1;
---
+++ src/pollCreate/index.js
@@ -0,0 +1,116 @@
+/**
+ * React Static Boilerplate
+ * https://github.com/kriasoft/react-static-boilerplate
+ *
+ * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import { request } from 'superagent';
+import classNames from 'classnames';
+import { Grid, Cell, Button } from 'react-mdl';
+import Layout from '../../components/Layout';
+import Step1 from '../../components/Step1';
+import s from './styles.css';
+
+
+// import Step2 from '../../components/Form/Step2';
+
+const MAX_STEPS = 3;
+
+class PollCreatePage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      steps: 1,
+      stepDetail: [],
+      resultUrl: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+    this.handlePrev = this.handlePrev.bind(this);
+  }
+
+  handleSubmit(event) {
+    //const { title, name, email, startDate, endDate } = this.refs.step1.refs;
+    console.log('fire', event.target);
+    /*
+    request
+      .post('/poll')
+      .send({
+        title: title.value,
+        name: name.value,
+        email: email.value,
+        startDate: startDate.value,
+        endDate: endDate.value,
+      });
+    */
+    this.handleNext();
+    event.preventDefault();
+  }
+
+  handleFinish() {
+    window.location = this.resultUrl;
+  }
+
+  handleNext(event) {
+    const steps = this.state.steps;
+    const nextSteps = (steps === MAX_STEPS) ? steps : steps + 1;
+    this.setState({ steps: nextSteps });
+    if (event) event.preventDefault();
+  }
+
+  handlePrev(event) {
+    const steps = this.state.steps;
+    const prevSteps = (steps === 1) ? steps : steps - 1;
+    this.setState({ steps: prevSteps });
+    event.preventDefault();
+  }
+
+  render() {
+    const stepClass = (step) => {
+      if (this.state.steps === step) {
+        return classNames(s.highlight);
+      }
+      return null;
+    };
+
+    return (
+      <Layout>
+        <h2>Create A Poll</h2>
+        <section id="state">
+          <span className={stepClass(1)}>1. General > </span>
+          <span className={stepClass(2)}>2. Time Proposals > </span>
+          <span className={stepClass(3)}>3. Invite </span>
+        </section>
+        <form onSubmit={this.handleSubmit}>
+          <section id="step-1" ref="step1" style={{ display: this.state.steps === 1 ? 'block' : 'none' }}>
+            <Step1 />
+          </section>
+          <section id="step-2" style={{ display: this.state.steps === 2 ? 'block' : 'none' }}>
+            {/* <Step2 startDate={this.refs.step1.refs.startDate} endDate={this.refs.step1.refs.endDate} /> */}
+          </section>
+          <section id="step-3" style={{ display: this.state.steps === 3 ? 'block' : 'none' }}>
+          </section>
+          <Grid>
+            <Cell col={3}>
+              <Button primary onClick={this.handlePrev}>Prev</Button>
+            </Cell>
+            <Cell col={3}>
+              <Button primary onClick={this.handleNext} style={{ display: this.state.steps !== 2 ? 'block' : 'none' }}>Next</Button>
+              <Button type="submit" style={{ display: this.state.steps === 2 ? 'block' : 'none' }}>Next</Button>
+            </Cell>
+            <Cell col={6}></Cell>
+          </Grid>
+        </form>
+      </Layout>
+    );
+  }
+
+}
+
+export default PollCreatePage;
