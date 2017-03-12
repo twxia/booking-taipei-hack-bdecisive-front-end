f1974b6f2d541316a9b8e9fec7f4bef982b84177
Angela Chuang <a830312@yahoo.com.tw>
2017-03-12 15:35:27 +0800

    add poll to routes

--- components/Vote/index.js
+++ components/Vote/index.js
@@ -1,11 +1,58 @@
 import React, { Component, PropTypes } from 'react'
 import moment from 'moment'
-import { get as _get } from 'lodash'
+import { get as _get, merge as _merge, clone as _clone } from 'lodash'
 import VoteByDate from './voteByDate'
+import request from 'superagent'
 
 class Vote extends Component {
+  constructor(props) {
+    super(props)
+    this._getPollData = this._getPollData.bind(this)
+    this._getHotelImages = this._getHotelImages.bind(this)
+    this.state = {
+      voteList: {}
+    }
+  }
+
+  componentDidMount() {
+    this._getPollData()
+  }
+
+  _getPollData(pollId = '7f1d14239e8d1b52f0c8debce9e8fc4f36f697ea') {
+    request.get(`http://taipeihack.patricks.tw/poll/${pollId}`)
+      .set({'Accept': 'application/json'})
+      .end((err, res) => {
+        if (err) return
+        let voteList =  _get(res, 'body', []),
+          hotelIdList = _clone(voteList.data).map((d, i) => d.booking_id).concat()
+        this._getHotelImages(hotelIdList)
+          .then((urls) => {
+            let data = voteList.data || []
+            _merge(data, urls)
+            voteList.data = data
+            this.setState({ voteList: voteList })
+          })
+      })
+
+  }
+
+  _getHotelImages(hotelId) {
+    return new Promise((resolve, reject) => {
+      request
+        .get(`https://distribution-xml.booking.com/json/bookings.getHotelDescriptionPhotos?hotel_ids=${hotelId}`)
+        .auth('hacker234', '8hqNW6HtfU')
+        .end((err, res) => {
+          const body = res.body;
+          const urls = body.map(d => ({ hotelData: {image: d.url_max300}}));
+
+          if (err) reject(err);
+          resolve(urls);
+        });
+    });
+  }
+
   render() {
-    let { voteList } = this.props,
+    let voteList = this.props.voteList,
       data = _get(voteList, 'data', []),
       title = _get(voteList, 'title', ''),
       startDate = _get(voteList, 'startDate', ''),
