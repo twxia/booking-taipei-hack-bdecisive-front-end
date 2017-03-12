89b395f0a3ddd8127ff482ca2becce12a75d170a
twxia <passion804222@gmail.com>
2017-03-12 10:37:29 +0800

    previewCard done

--- components/Step2/previewCard.js
+++ components/Step2/previewCard.js
@@ -5,39 +5,60 @@ import { classNames } from 'classnames';
 class PreviewCard extends Component {
   constructor(props) {
     super(props);
+    this.intervalId = 0;
     this.state = {
       captionNum: 0,
+      imageUrls: this.props.imageUrls,
     };
+    this.changeImageUrl = this.changeImageUrl.bind(this);
+    this.goToUrl = this.goToUrl.bind(this);
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(this.changeImageUrl, 2500);
+
+    this.intervalId = intervalId;
+  }
+
+  componentWillUnmount() {
+    // use intervalId from the state to clear the interval
+    clearInterval(this.intervalId);
+  }
+
+  changeImageUrl() {
+    const imageUrls = this.props.imageUrls;
+    const captionNum = this.state.captionNum;
+
+    this.setState({ captionNum: (captionNum >= imageUrls.length - 1) ? 0 : captionNum + 1 });
+  }
+
+  goToUrl(event) {
+    console.log(this.props);
+    window.open(this.props.info.url, '_blank');
+    event.preventDefault();
   }
 
   render() {
-    
     const captionNum = this.state.captionNum;
     const imageUrls = this.props.imageUrls;
     const styles = {
-        color: '#fff',
-        height: '176px',
-        background: `url(${imageUrls[captionNum]}) center / cover`,
-      };
+      color: '#fff',
+      height: '176px',
+      background: `url(${imageUrls[captionNum]}) center / cover`,
+      transition: 'background .8s',
+    };
 
-    setInterval(() => {
-      this.setState({ captionNum: (captionNum >= imageUrls.length - 1) ? 0 : captionNum + 1 });
-    }, 2500);
-
+    const { name, address } = this.props.info;
 
     return (
       <div>
         <Card shadow={0} style={{ width: '512px', margin: 'auto' }}>
-          <CardTitle style={styles}>Welcome</CardTitle>
+          <CardTitle style={styles}>{name}</CardTitle>
           <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Mauris sagittis pellentesque lacus eleifend lacinia...
-    </CardText>
-          <CardActions border>
-            <Button colored>Get Started</Button>
-          </CardActions>
+            <p>address: {address}</p>
+          </CardText>
           <CardMenu style={{ color: '#fff' }}>
-            <IconButton name="share" />
+            <IconButton type="button" name="share" onClick={this.goToUrl} />
           </CardMenu>
         </Card>
       </div>
@@ -46,7 +67,8 @@ class PreviewCard extends Component {
 }
 
 PreviewCard.propTypes = {
-  imageUrls: React.PropTypes.Array,
+  imageUrls: React.PropTypes.array,
+  info: React.PropTypes.object,
 };
 
 export default PreviewCard;
